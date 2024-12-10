a74122e133499d825bddf18ed34f84b195a6d124
tman689 <tranminhan05092000@gmail.com>
2024-12-10 21:14:14 +0700

    hoàn thành bài tập 4

---
+++ lap trinh c ss2 bai 3.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	int canh = 10;
+	
+	int cv = 4 * canh;
+	int dt = canh * canh;
+	
+	printf("Chu vi hinh vuong : %d\n", cv);
+	printf("dien tich hinh vuong : %d\n", dt);
+	
+	return 0;
+	
+}
